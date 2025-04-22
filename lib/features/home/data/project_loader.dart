2bdafa6e6ea5605bdededff0c1c817dd3e7bc5c3
zaid-kamil <xaidmetamorphos@gmail.com>
2025-04-22 12:30:18 +0530

    Refactor Project instances to use 'const' for improved performance and consistency

--- lib/features/home/data/project_loader.dart
+++ lib/features/home/data/project_loader.dart
@@ -3,7 +3,7 @@ import 'package:zbk_portfolio/features/home/domain/project.dart';
 class ProjectLoader {
   Future<List<Project>> loadProjects() async {
     return [
-      Project(
+      const Project(
         category: ProjectCategory.webApp,
         title: 'ZBK Portfolio',
         description: 'My personal portfolio app',
@@ -13,7 +13,6 @@ class ProjectLoader {
         url: "https://zaidkamil.socialmistry.com",
         difficultyLevel: 5,
       ),
-      // New data science projects
       const Project(
         category: ProjectCategory.dataScience,
         title: 'Dputils Library',
@@ -25,7 +24,6 @@ class ProjectLoader {
         imageUrl: 'assets/images/dputils.png',
         difficultyLevel: 8,
       ),
-
       const Project(
           category: ProjectCategory.dataScience,
           title: 'Pit - Python Based Version Control System',
@@ -36,7 +34,6 @@ class ProjectLoader {
           imageUrl: 'assets/images/pit.png',
           url: "https://pypi.org/project/pit",
           difficultyLevel: 6),
-
       const Project(
           category: ProjectCategory.webApp,
           title: 'Train Easy – AI Model Training Pipeline',
@@ -49,7 +46,6 @@ class ProjectLoader {
           screenshots: [
             'assets/images/train_easy.png',
           ]),
-
       const Project(
           category: ProjectCategory.webApp,
           title: 'Virtual Assistance for the Blind',
@@ -68,7 +64,6 @@ class ProjectLoader {
               "https://github.com/zaid-kamil/Virtual-Assistance-For-The-Blind",
           imageUrl: 'assets/images/blind_assist.png',
           difficultyLevel: 7),
-
       const Project(
           category: ProjectCategory.dataScience,
           title: 'Amazon Reviews Analysis',
@@ -84,7 +79,6 @@ class ProjectLoader {
           githubUrl: "https://github.com/zaid-kamil/amazon-reviews-analysis",
           imageUrl: 'assets/images/amazon_reviews.png',
           difficultyLevel: 6),
-
       const Project(
           category: ProjectCategory.mobileApp,
           title: 'Fiery-App',
@@ -98,7 +92,6 @@ class ProjectLoader {
             'assets/images/fiery_app.png',
             'assets/images/fiery_app2.png',
           ]),
-
       const Project(
           category: ProjectCategory.mobileApp,
           title: 'SpeedTest App',
@@ -108,7 +101,6 @@ class ProjectLoader {
           githubUrl: "https://github.com/zaid-kamil/SpeedTest-compose",
           imageUrl: 'assets/images/speedtest_app.jpg',
           difficultyLevel: 6),
-
       const Project(
           category: ProjectCategory.mobileApp,
           title: 'Diabetes Classifier App',
@@ -118,7 +110,6 @@ class ProjectLoader {
           githubUrl: "https://github.com/zaid-kamil/diabetes_classifier_app",
           imageUrl: 'assets/images/diabetes.png',
           difficultyLevel: 7),
-
       const Project(
           category: ProjectCategory.mobileApp,
           title: 'House Price Prediction App',
@@ -129,7 +120,6 @@ class ProjectLoader {
               "https://github.com/zaid-kamil/flutter_house_price_prediction",
           imageUrl: 'assets/images/house_price.png',
           difficultyLevel: 8),
-
       const Project(
           category: ProjectCategory.mobileApp,
           title: 'Image Recognition App',
@@ -139,8 +129,7 @@ class ProjectLoader {
           githubUrl: "https://github.com/zaid-kamil/image_recognition_app",
           imageUrl: 'assets/images/image_recognition.png',
           difficultyLevel: 6),
-      // certificates
-      Project(
+      const Project(
         title: 'Cloud Technical Series AI Deep Dive',
         description:
             'Issued by Google Cloud Asia Pacific\nCredential ID: 133127404',
@@ -148,7 +137,7 @@ class ProjectLoader {
         technologies: ['Cloud', 'AI'],
         url: "https://www.credential.net/77e0442e-960c-44fc-a64b-f2826a4b2817",
       ),
-      Project(
+      const Project(
         title: 'Google Play Store Listing Certificate',
         description:
             'Issued by Google Play Academy\nCredential ID: 128561205\nValid: Jan 2025 - Jan 2028',
@@ -157,7 +146,7 @@ class ProjectLoader {
         url: "https://www.credential.net/3420b5d5-cc71-40f5-81fe-ca08900e2990",
         imageUrl: "assets/images/google_play_academy.png",
       ),
-      Project(
+      const Project(
         title: 'Introduction to Data Analytics on Google Cloud',
         description: 'Issued by Google\nCredential ID: 13472157',
         category: ProjectCategory.certificates,
@@ -165,7 +154,7 @@ class ProjectLoader {
         url:
             "https://www.cloudskillsboost.google/public_profiles/23b77b8a-b1b1-44fb-9139-725c75903f2e/badges/13472157?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
       ),
-      Project(
+      const Project(
         title: 'Python Essentials 1',
         description: 'Issued by Cisco',
         category: ProjectCategory.certificates,
@@ -174,7 +163,7 @@ class ProjectLoader {
             "https://www.credly.com/badges/ba779349-5e49-4544-b39f-b192d31dbe2f/linked_in_profile",
         imageUrl: "assets/images/python_essentials.png",
       ),
-      Project(
+      const Project(
         title: 'Data Science 101',
         description:
             'Issued by IBM SkillsBuild\nCredential ID: e14b11894ad346418d4106f4543640f8',
@@ -183,7 +172,7 @@ class ProjectLoader {
         url:
             "https://courses.skillsbuild.skillsnetwork.site/certificates/e14b11894ad346418d4106f4543640f8",
       ),
-      Project(
+      const Project(
         title: 'Data Science Tools',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -196,7 +185,7 @@ class ProjectLoader {
         url:
             "https://www.credly.com/badges/14c409ef-cc8d-49e3-8514-7fbb155575f3/linked_in_profile",
       ),
-      Project(
+      const Project(
         title: 'Machine Learning with Python - Level 1',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -205,7 +194,7 @@ class ProjectLoader {
             "https://www.credly.com/badges/3d663787-b600-45a2-b799-eb7f47149001/linked_in_profile",
         imageUrl: "assets/images/machine_learning_with_python.png",
       ),
-      Project(
+      const Project(
         title: 'SQL and Relational Databases 101',
         description:
             'Issued by IBM SkillsBuild\nCredential ID: 5092a316a3c44cabbb8809107eea8983',
@@ -214,7 +203,7 @@ class ProjectLoader {
         url:
             "https://courses.skillsbuild.skillsnetwork.site/certificates/5092a316a3c44cabbb8809107eea8983",
       ),
-      Project(
+      const Project(
         title: 'Prompt Engineering for Everyone',
         description:
             'Issued by IBM SkillsNetwork\nCredential ID: course-v1:IBMSkillsNetwork AI0117EN v1',
@@ -223,7 +212,7 @@ class ProjectLoader {
         url:
             "https://courses.cognitiveclass.ai/certificates/bb44e48225754e7e8cf23591cf90fbc6",
       ),
-      Project(
+      const Project(
         title: 'Automate ML Pipelines Using Apache Airflow',
         description:
             'Issued by IBM\nCredential ID: course-v1:IBM GPXX0DNQEN v1',
@@ -232,7 +221,7 @@ class ProjectLoader {
         url:
             "https://courses.cognitiveclass.ai/certificates/5acf353a8b764f74a7e20a2599ae1ddf",
       ),
-      Project(
+      const Project(
         title: 'Image Segmentation with Mean Shift Clustering',
         description:
             'Issued by IBM SkillsNetwork\nCredential ID: course-v1:IBMSkillsNetwork GPXX04YGEN v1',
@@ -241,7 +230,7 @@ class ProjectLoader {
         url:
             "https://courses.cognitiveclass.ai/certificates/17645098c32d440999b4fe64ef02f025",
       ),
-      Project(
+      const Project(
         title: 'Enterprise Design Thinking Practitioner',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -250,7 +239,7 @@ class ProjectLoader {
             "https://www.credly.com/badges/9c7aa05c-6ac0-4c51-82e6-82c9aa0019d9/linked_in_profile",
         imageUrl: "assets/images/enterprise_design_thinking.png",
       ),
-      Project(
+      const Project(
         title: 'Cloud Core',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -258,7 +247,7 @@ class ProjectLoader {
         url:
             "https://www.credly.com/badges/f784cddf-806e-4f50-930c-fd617d83834a/linked_in_profile",
       ),
-      Project(
+      const Project(
         title: 'IBM Cloud Essentials',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -267,7 +256,7 @@ class ProjectLoader {
             "https://www.credly.com/badges/43555279-4cfa-449b-a929-d0ae21dc0c48/linked_in_profile",
         imageUrl: "assets/images/ibm_cloud_essentials.png",
       ),
-      Project(
+      const Project(
         title: 'Applied Data Science with Python - Level 2',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -275,7 +264,7 @@ class ProjectLoader {
         url:
             "https://www.credly.com/badges/52283130-4adf-449f-9943-68e576b5a1c5/linked_in_profile",
       ),
-      Project(
+      const Project(
         title: 'Data Analysis Using Python',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -284,7 +273,7 @@ class ProjectLoader {
             "https://www.credly.com/badges/63827ced-b04d-428c-b537-5a50837c913b/linked_in_profile",
         imageUrl: "assets/images/data_analysis_using_python.png",
       ),
-      Project(
+      const Project(
         title: 'Data Science Foundations - Level 2 (V2)',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -294,7 +283,7 @@ class ProjectLoader {
         imageUrl:
             "assets/images/data_science_foundations_with_python_level_2.png",
       ),
-      Project(
+      const Project(
         title: 'Data Science Methodologies',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -302,7 +291,7 @@ class ProjectLoader {
         url:
             "https://www.credly.com/badges/ae263276-176d-4a9c-9fad-7d9527660b67/linked_in_profile",
       ),
-      Project(
+      const Project(
         title: 'Data Visualization Using Python',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -310,7 +299,7 @@ class ProjectLoader {
         url:
             "https://www.credly.com/badges/0559521a-e89f-4593-94a3-557350448e86/linked_in_profile",
       ),
-      Project(
+      const Project(
         title: 'Python for Data Science',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -319,7 +308,7 @@ class ProjectLoader {
             "https://www.credly.com/badges/c8a0b1f9-ecae-4980-9ab1-aa51c5ede18b/linked_in_profile",
         imageUrl: "assets/images/python_for_data_science.png",
       ),
-      Project(
+      const Project(
         title: 'Data Science Foundations - Level 1',
         description: 'Issued by IBM',
         category: ProjectCategory.certificates,
@@ -327,7 +316,7 @@ class ProjectLoader {
         url:
             "https://www.credly.com/badges/0bd41153-7e17-48ca-870d-0db4feb47565/linked_in_profile",
       ),
-      Project(
+      const Project(
         title: 'Associate Android Developer',
         description:
             'Issued by Google for Developers\nCredential ID: 10943363\nValid: May 2017 - May 2020',
@@ -346,21 +335,40 @@ class ProjectLoader {
             "https://www.credential.net/u2ufqxxm?key=1e566317c54f4f4aa751f9a229f9da9d2975e31aa3e8ab28f4efcf1001f9d4bd",
         imageUrl: "assets/images/associate_android_developer.png",
       ),
-      Project(
+      const Project(
+        category: ProjectCategory.dataScience,
+        // Replace with an actual ProjectCategory enum value
+        title: "Automobile Dashboard App",
+        description:
+            "The app provides various functionalities to visualize and explore the data, including numerical analysis, textual analysis, and correlation analysis.",
+        technologies: ["Python", "Pandas", "Plotly", "Streamlit"],
+        imageUrl: "assets/images/automobile_dashboard.png",
+        githubUrl: "https://github.com/zaid-kamil/Automobile_Dashboard_App",
+        url:
+            "https://zaid-kamil-automobile-dashboard-app-app-zuc0at.streamlit.app/",
+        difficultyLevel: 3,
+        screenshots: [
+          "assets/images/automobile_dashboard.png",
+          "assets/images/automobile_dashboard2.png",
+          "assets/images/automobile_dashboard3.png",
+        ],
+        videoUrl: "https://www.youtube.com/watch?v=SqKjYOzeKZw",
+      ),
+      const Project(
         title: 'Problem Solving (Basic)',
         description: 'Issued by HackerRank',
         category: ProjectCategory.certificates,
         technologies: ['Data Structures'],
         url: "https://www.hackerrank.com/certificates/df10e7f8163d",
       ),
-      Project(
+      const Project(
         title: 'Python (Basic)',
         description: 'Issued by HackerRank',
         category: ProjectCategory.certificates,
         technologies: ['Python Programming'],
         url: "https://www.hackerrank.com/certificates/4d6fa264282e",
       ),
-      Project(
+      const Project(
           title: 'SQL (Intermediate)',
           description: 'Issued by HackerRank',
           category: ProjectCategory.certificates,
